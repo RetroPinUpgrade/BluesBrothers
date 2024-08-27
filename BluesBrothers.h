a5341d7779d54bc1ce50fb40f86b1f83c9013928
Dick Hamill <36781010+MrEkted@users.noreply.github.com>
2024-08-27 00:00:32 -0400

    Add files via upload

---
+++ BluesBrothers.h
@@ -0,0 +1,104 @@
+// These example lamp definitions come from Supersonic
+#define LAMP_SHOOT_AGAIN            35    // Q56
+#define LAMP_LEFT_OUTLANE           2     // Q36
+#define LAMP_LEFT_INLANE            46    // Q39
+#define LAMP_RIGHT_INLANE           7     // Q50
+#define LAMP_RIGHT_OUTLANE_EB       11    // Q51
+#define LAMP_OPEN_SAVE_GATE         38    // Q41
+#define LAMP_LEFT_VUK               34    // Q42
+#define LAMP_LOOP_BONUS             39    // Q46
+#define LAMP_BONUS_1                18    // Q48
+#define LAMP_BONUS_2                3     // Q57
+#define LAMP_BONUS_3                26    // Q32
+#define LAMP_BONUS_4                47    // Q53
+#define LAMP_BONUS_5                42    // Q40
+#define LAMP_BONUS_6                14    // Q49
+#define LAMP_SPECIAL                10    // Q44
+#define LAMP_LEFT_SLING             6     // Q38
+#define LAMP_RIGHT_SLING            43    // Q52
+#define LAMP_CAPTIVE_BALL           45    // Q21
+#define LAMP_E_1                    44    // Q7
+#define LAMP_L                      36    // Q2
+#define LAMP_W                      4     // Q12
+#define LAMP_LEFT_SPINNER           0     // Q14
+#define LAMP_O_1                    37    // Q17
+#define LAMP_O_2                    9     // Q28
+#define LAMP_D                      13    // Q35
+#define LAMP_E_2                    41    // Q23
+#define LAMP_K                      33    // Q24
+#define LAMP_A                      1     // Q29
+#define LAMP_J                      5     // Q27
+#define LAMP_HIDE_AWAY              17    // Q34
+#define LAMP_POP_BLUE_UP            28    // Q4
+#define LAMP_POP_WHITE_DOWN         12    // Q8
+#define LAMP_POP_SMALL_BLUE         20    // Q10
+#define LAMP_BEHIND_CAPTIVE         8     // Q13
+
+
+
+#define SW_LEFT_SPINNER             0
+#define SW_RIGHT_SPINNER            1
+#define SW_TROUGH_1                 2
+#define SW_TROUGH_2                 3
+#define SW_TROUGH_3                 4
+#define SW_OUTHOLE                  7
+#define SW_CREDIT_RESET             8
+#define SW_COIN_1                   9
+#define SW_LAUNCH                   10
+#define SW_TILT                     11
+#define SW_PLUMB_TILT               11
+#define SW_ROLL_TILT                11
+#define SW_MID_TOP                  12
+#define SW_CAPTIVE_BALL             13
+#define SW_MID_LEFT                 14
+#define SW_BOTTOM_LEFT              15
+#define SW_DROP_R_1                 16
+#define SW_DROP_R_2                 17
+#define SW_DROP_R_3                 18
+#define SW_LEFT_SLING               19
+#define SW_RIGHT_SLING              20
+#define SW_TOP_SLING                21
+#define SW_LEFT_FLIPPER             22
+#define SW_RIGHT_FLIPPER            23
+#define SW_DROP_L_1                 24
+#define SW_DROP_L_2                 25
+#define SW_DROP_L_3                 26
+#define SW_STANDUP_J                27
+#define SW_STANDUP_A                28
+#define SW_STANDUP_K                29
+#define SW_STANDUP_E                30
+#define SW_POP_BUMPER               31
+#define SW_VUK                      32
+#define SW_SHOOTER_LANE             33
+#define SW_LEFT_OUTLANE             34
+#define SW_LEFT_INLANE              35
+#define SW_RIGHT_OUTLANE            36
+#define SW_RIGHT_INLANE             37
+#define SW_RIGHT_FLIP_LANE          38
+#define SW_TRAP                     39
+
+#define SOL_TOP_GATE_OPEN           1
+#define SOL_TOP_GATE_CLOSED         0
+#define SOL_LIFT_GATE               2
+#define SOL_POP_BUMPER              6
+#define SOL_DROP_BANK_R_RESET       2
+#define SOL_DROP_BANK_L_RESET       3
+#define SOL_KNOCKER                 4
+#define SOL_VUK                     7
+#define SOL_LEFT_SLING              10
+#define SOL_RIGHT_SLING             11
+#define SOL_TOP_SLING               13
+#define SOL_SHOOTER_KICK            12
+#define SOL_SERVE_BALL              8
+#define SOL_LEFT_GATE_OPEN          5
+
+
+#define NUM_SWITCHES_WITH_TRIGGERS          4 // total number of solenoid/switch pairs
+#define NUM_PRIORITY_SWITCHES_WITH_TRIGGERS 4 // This number should match the define above
+
+struct PlayfieldAndCabinetSwitch SolenoidAssociatedSwitches[] = {
+  { SW_RIGHT_SLING, SOL_RIGHT_SLING, 4},
+  { SW_LEFT_SLING, SOL_LEFT_SLING, 4},
+  { SW_TOP_SLING, SOL_TOP_SLING, 4},
+  { SW_POP_BUMPER, SOL_POP_BUMPER, 4}
+};
---
+++ DropTargets.h
@@ -0,0 +1,183 @@
+// This file must be included after RPU_config.h
+
+#define DROP_TARGET_BANK_CLEARED          1
+#define DROP_TARGET_BANK_CLEARED_IN_ORDER 2
+
+#define DROP_TARGET_TYPE_BLY_1          0   // no solenoids to drop individual targets, one switch per target
+#define DROP_TARGET_TYPE_STRN_1         1   // no solenoids to drop individual targets, one switch per target
+#define DROP_TARGET_TYPE_STRN_2         2   // clearing solenoids for individual targets, one switch per target (aka memory drops)
+#define DROP_TARGET_TYPE_WLLMS_1        3   // no solenoids to drop individual targets, one switch per target (momentary), switch for all targets down
+#define DROP_TARGET_TYPE_WLLMS_2        4   // no solenoids to drop/reset individual, one switch per target
+#define DROP_TARGET_TYPE_HOMEPIN_1      5   // no solenoids to drop individual targets, one switch per target
+
+class DropTargetBank
+{
+  public:
+    DropTargetBank(byte s_numSwitches, byte s_numSolenoids, byte s_bankType, byte s_solenoidOnTime);
+    ~DropTargetBank();
+    void DefineSwitch(byte switchOrder, byte switchNum);
+    void DefineResetSolenoid(byte solIndex, byte solChannelNumber);
+    void AddAllTargetsSwitch(byte s_allTargetsSwitch);
+    byte HandleDropTargetHit(byte switchNum);
+    byte CheckIfBankCleared();
+    void Update(unsigned long currentTime);
+    void ResetDropTargets(unsigned long timeToReset, boolean ignoreQuickDrops=false);
+    byte GetStatus(boolean readSwitches = true);
+
+  private:
+    byte numSwitches;
+    byte bankType;
+    byte numSolenoids;
+    byte *switchArray;
+    byte *solArray;
+    byte allTargetsSwitch;
+    byte solenoidOnTime;
+    byte bankStatus;
+    byte bankBitmask;
+    unsigned long targetResetTime;
+    unsigned long ignoreDropsUntilTime;
+    boolean bankCleared;
+    boolean targetsHitInOrder;
+    byte numTargetsInOrder;
+};
+
+DropTargetBank::DropTargetBank(byte s_numSwitches, byte s_numSolenoids, byte s_bankType, byte s_solenoidOnTime) {
+  numSwitches = s_numSwitches;
+  numSolenoids = s_numSolenoids;
+  solenoidOnTime = s_solenoidOnTime;
+  bankType = s_bankType;
+  if (numSwitches) switchArray = new byte[numSwitches];
+  if (numSolenoids) solArray = new byte[numSolenoids];
+  allTargetsSwitch = 0xFF;
+  bankStatus = 0;
+  bankBitmask = 0;
+  bankCleared = false;
+  targetsHitInOrder = true;
+  numTargetsInOrder = 0;
+
+  for (byte count=0; count<numSwitches; count++) {
+    switchArray[count] = 0xFF;
+    bankBitmask *= 2;
+    bankBitmask |= 1;
+  }
+  for (byte count=0; count<numSolenoids; count++) solArray[count] = 0xFF;
+
+  targetResetTime = 0;
+  ignoreDropsUntilTime = 0;
+}
+
+DropTargetBank::~DropTargetBank() {
+  delete switchArray;
+  delete solArray;
+}
+
+void DropTargetBank::DefineSwitch(byte switchOrder, byte switchNum) {
+  if (switchOrder>=numSwitches) return;
+  switchArray[switchOrder] = switchNum;
+}
+
+void DropTargetBank::AddAllTargetsSwitch(byte s_allTargetsSwitch) {
+  if (bankType!=DROP_TARGET_TYPE_WLLMS_1) return;  
+  allTargetsSwitch = s_allTargetsSwitch;
+}
+
+void DropTargetBank::DefineResetSolenoid(byte solIndex, byte solChannelNumber) {
+  if (solIndex>=numSolenoids) return;
+  solArray[solIndex] = solChannelNumber;
+}
+
+
+byte DropTargetBank::HandleDropTargetHit(byte switchNum) {
+  byte oldStatus = bankStatus;
+
+  byte targetBits = 0x00;
+  byte singleBit = 0x01;
+
+  if (ignoreDropsUntilTime) {
+    return 0;
+  }
+  
+  for (byte count=0; count<numSwitches; count++) {
+    if (switchNum==switchArray[count]) {
+      // If this is a new hit, see if it's in order
+      if ((bankStatus & singleBit)==0x00) {
+        if (targetsHitInOrder && count==numTargetsInOrder) {
+          numTargetsInOrder += 1;        
+        } else {
+          targetsHitInOrder = false;
+          numTargetsInOrder = 0;
+        }
+      }
+      targetBits |= singleBit;
+      break;
+    }
+    singleBit *= 2;
+  }
+
+  if (allTargetsSwitch!=0xFF) {
+    if (RPU_ReadSingleSwitchState(allTargetsSwitch) || switchNum==allTargetsSwitch) targetBits = bankBitmask & (~bankStatus);
+  }
+
+  bankStatus |= targetBits;
+  if (bankStatus==bankBitmask) bankCleared = true;
+
+  return bankStatus & ~oldStatus;
+}
+
+byte DropTargetBank::CheckIfBankCleared() {
+  if (bankCleared) {
+    if (targetsHitInOrder) return DROP_TARGET_BANK_CLEARED_IN_ORDER;
+    else return DROP_TARGET_BANK_CLEARED;
+  }   
+  return 0;
+}
+
+byte DropTargetBank::GetStatus(boolean readSwitches) {
+  if (readSwitches) {
+    byte bitMask = 0x01;
+    byte returnStatus = 0x00;
+    for (byte count=0; count<numSwitches; count++) {
+      if (RPU_ReadSingleSwitchState(switchArray[count])) returnStatus |= bitMask;
+      bitMask *= 2;
+    }
+    return returnStatus;
+  } else {
+    return bankStatus;
+  }
+}
+
+
+void DropTargetBank::ResetDropTargets(unsigned long timeToReset, boolean ignoreQuickDrops) {
+  bankCleared = false;
+  targetsHitInOrder = true;
+  numTargetsInOrder = 0;
+
+  if (targetResetTime) {
+    // We've already requested this bank to reset, so don't queue it again
+    return;    
+  }
+
+  if (numSolenoids) {
+    for (byte count=0; count<numSolenoids; count++) {
+      if (solArray[count]!=0xFF) RPU_PushToTimedSolenoidStack(solArray[count], solenoidOnTime, timeToReset);
+    }
+    targetResetTime = timeToReset + 100; // This could be based on solenoidOnTime, but that's not currently set in ms
+    if (ignoreQuickDrops) {
+      ignoreDropsUntilTime = targetResetTime + 100;
+    } else {
+      ignoreDropsUntilTime = 0;
+    }
+  }
+
+  return;
+}
+
+void DropTargetBank::Update(unsigned long currentTime) {
+  if (targetResetTime && currentTime>targetResetTime) {
+    bankStatus = GetStatus();
+    targetResetTime = 0;
+  }
+  if (ignoreDropsUntilTime && currentTime>ignoreDropsUntilTime) {
+    ignoreDropsUntilTime = 0;
+  }
+}
